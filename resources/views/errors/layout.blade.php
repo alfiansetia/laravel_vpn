495abaf4262d09551cb60ab96658574861542d15
alfiansetia <alfian.setia100@gmail.com>
2023-10-05 08:01:27 +0700

    error page

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,53 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <title>@yield('title')</title>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .title {
-                font-size: 36px;
-                padding: 20px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title">
-                    @yield('message')
-                </div>
+        .position-ref {
+            position: relative;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 36px;
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title">
+                @yield('message')
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -432,6 +432,83 @@
                 }
             }
 
+            function deleteData(url) {
+                if (selected()) {
+                    swal({
+                        title: 'Delete Selected Data?',
+                        text: "You won't be able to revert this!",
+                        type: 'warning',
+                        showCancelButton: true,
+                        confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yes!',
+                        confirmButtonAriaLabel: 'Thumbs up, Yes!',
+                        cancelButtonText: '<i class="fa fa-thumbs-down"></i> No',
+                        cancelButtonAriaLabel: 'Thumbs down',
+                        padding: '2em',
+                        animation: false,
+                        customClass: 'animated tada',
+                    }).then(function(result) {
+                        if (result.value) {
+                            let form = $("#formSelected");
+                            $.ajaxSetup({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                }
+                            });
+                            $.ajax({
+                                type: 'DELETE',
+                                url: url,
+                                data: $(form).serialize(),
+                                beforeSend: function() {
+                                    block();
+                                },
+                                success: function(res) {
+                                    unblock();
+                                    table.ajax.reload();
+                                    if (res.status == true) {
+                                        swal(
+                                            'Success!',
+                                            res.message,
+                                            'success'
+                                        )
+                                    } else {
+                                        swal(
+                                            'Failed!',
+                                            res.message,
+                                            'error'
+                                        )
+                                    }
+                                },
+                                error: function(xhr, status, error) {
+                                    unblock();
+                                    er = xhr.responseJSON.errors
+                                    if (xhr.status == 403) {
+                                        swal(
+                                            'Failed!',
+                                            xhr.responseJSON.message,
+                                            'error'
+                                        )
+                                    } else if (xhr.status == 422) {
+                                        swal(
+                                            'Failed!',
+                                            xhr.responseJSON.message,
+                                            'error'
+                                        )
+                                    } else if (xhr.status == 401) {
+                                        window.location.reload()
+                                    } else {
+                                        swal(
+                                            'Failed!',
+                                            'Server Error',
+                                            'error'
+                                        )
+                                    }
+                                }
+                            });
+                        }
+                    })
+                }
+            }
+
         });
     </script>
 @endpush
