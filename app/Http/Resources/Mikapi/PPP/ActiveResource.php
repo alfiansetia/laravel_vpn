80e9b2231b99617aeeb4530b7f6ca9f2e5251504
alfiansetia <alfian.setia100@gmail.com>
2023-10-11 08:01:57 +0700

    add mikapi resource

---
+++ app/Http/Resources/Mikapi/PPP/ActiveResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Mikapi\PPP;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActiveResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
